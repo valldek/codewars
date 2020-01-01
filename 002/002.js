0db752a9a59ca7ec43ab75e4b78cea7103edf1a5
valldek <valldek@gmail.com>
2020-01-01 08:26:02 +0100

    Second shoot

---
+++ 002/002.js
@@ -0,0 +1,13 @@
+function likes(names) {
+  if (names.length > 3) {
+    return `${names.slice(0, 2).join(', ')} and ${names.length - 2} others like this`;
+  } else if (names.length === 3) {
+    return `${names.slice(0, 2).join(', ')} and ${names[2]} like this`;
+  } else if (names.length === 1) {
+    return `${names} likes this`;
+  } else if (names.length === 0) {
+    return 'no one likes this';
+  } else {
+    return `${names.join(' and ')} like this`;
+  }
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # [Codewars](https://www.codewars.com) solutions
 
 * 001 - Multiply `8kyu`
+* 002 - Who likes it? `6kyu`
