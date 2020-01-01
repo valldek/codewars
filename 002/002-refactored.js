f4105b3cdfbb7c29600136f54806ee09ff372dcd
valldek <valldek@gmail.com>
2020-01-01 08:29:50 +0100

    Second shoot refactored

---
+++ 002/002-refactored.js
@@ -0,0 +1,14 @@
+function likes(names) {
+  switch (names.lenght) {
+    case 0:
+      return 'no one likes this';
+    case 1:
+      return `${names} likes this`;
+    case 2:
+      return `${names.join(' and ')} like this`;
+    case 3:
+      return `${names.slice(0, 2).join(', ')} and ${names[2]} like this`;
+    default:
+      return `${names.slice(0, 2).join(', ')} and ${names.length - 2} others like this`;
+  }
+}
