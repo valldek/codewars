04a5a97e46257ed429316303ee08d79507649019
valldek <valldek@gmail.com>
2020-01-07 21:32:22 +0100

    11. Input checking

--- 011/011.js
+++ 011/011.js
@@ -1,4 +1,7 @@
 function divisors (integer) {
+  if (!Number.isFinite(integer)) return 'Wrong Input - not an integer';
+  if (integer < 0) return 'Wrong Input - less then 0';
+
   const divs = Math.floor(integer / 2);
   let divisorsArray = [];
 
