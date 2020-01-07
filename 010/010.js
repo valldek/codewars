65e8391396816ff6dcacb03c727397e82ae8b4b0
valldek <valldek@gmail.com>
2020-01-07 20:59:26 +0100

    10. Solution - update

--- 010/010.js
+++ 010/010.js
@@ -1,7 +1,9 @@
 function squareDigits (num) {
+  if (!Number.isFinite()) return 'Wrong Input';
+
   const digitsString = num.toString().split('');
-  const digitsNumber = digitsString.map((val) => parseInt(val));
+  const digitsNumber = digitsString.map((val) => parseInt(val, 10));
   const digitsSquared = digitsNumber.map((val) => val*val);
 
-  return parseInt(digitsSquared.join(''));
+  return parseInt(digitsSquared.join(''), 10);
 }
