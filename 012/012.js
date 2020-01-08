afcd5aef412f16ca2d537030af8bf92892e80ac8
valldek <valldek@gmail.com>
2020-01-08 09:21:36 +0100

    012 Solution - input checks

--- 012/012.js
+++ 012/012.js
@@ -1,4 +1,9 @@
 function deleteNth (arr, n) {
+  if (!Array.isArray(arr)) return 'Wrong Input - arr is not an array';
+  if (!arr.length) return [];
+  if (!Number.isFinite(n)) return 'Wrong Input - n is not a Finite number';
+  if (n < 0) return 'Wrong Input - n is not positive integer';
+
   const occurences = {};
 
   return arr.filter( (val) => {
