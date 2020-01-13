9c295eaf6b571de42000c2631186aaf698e1b980
valldek <valldek@gmail.com>
2020-01-13 10:04:24 +0100

    018 ensure lowercase and string input

--- 018/018.js
+++ 018/018.js
@@ -1,6 +1,8 @@
 function high (x) {
+  if (typeof x !== 'string') return 'Wrong input';
+  const loweredString = x.toLowerCase();
 
-  const wordsArray = x.split(' ');
+  const wordsArray = loweredString.split(' ');
   const wordsScore = wordsArray.map((val) => {
     return val.split('').reduce((acc, cur) => {
       return acc += cur.charCodeAt() - 96;
