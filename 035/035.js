fe396bd1fa15d9cde8b2f1d30a205e13ba566059
valldek <valldek@gmail.com>
2020-01-29 07:27:29 +0100

    035 Solution

--- 035/035.js
+++ 035/035.js
@@ -2,13 +2,13 @@ const MORSE_CODE = require('./MORSE_CODE');
 
 const decodeMorse = function (morseCode) {
   const w = morseCode.split('   ');
-  return w.map((val) => {
-    return val.split('  ')
-      .map((v) => {
-        return MORSE_CODE[v]
-      })
-      .join('')
-  })
-  .join(' ')
-  .trim();
+  return w.
+    map((val) =>
+      (val.split('  ')
+        .map((v) => {
+          return MORSE_CODE[v]
+        })
+      ).join(''))
+      .join(' ')
+      .trim();
 }
